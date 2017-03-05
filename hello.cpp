f43e136954ba2a95d26ca2c1e7878ebf139370cc
Edson Monteiro <edsonmonteiro1995@gmail.com>
2017-03-05 09:59:38 -0300

    Terceiro Commit

--- hello.cpp
+++ hello.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main () {
 
-	cout << "Hello !" << endl;
+	cout << "Hello World!" << endl;
+	int a=2, b =3, c;
+	c = a + b;
+	cout << "o valor de C eh: " << c << endl;
 	return 0;
 }
